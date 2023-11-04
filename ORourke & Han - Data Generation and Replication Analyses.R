594bb054cce0eadddd8d32cc7b0b690080b1e0d6
horourke <horourke@asu.edu>
2023-11-04 11:58:11 -0700

    Update ORourke & Han - Data Generation and Replication Analyses.R
    Directory edits

--- ORourke & Han - Data Generation and Replication Analyses.R
+++ ORourke & Han - Data Generation and Replication Analyses.R
@@ -1,5 +1,6 @@
 #O'Rourke & Han (2023) "Considering the Distributional Form of Zeroes When Calculating Mediation Effects with Zero-Inflated Count Outcomes"
 # R MplusAutomation for data generation and replication analysis scripts
+# Ensure you have administrative permission to copy and remove files before running
 
 
 install.packages("MplusAutomation")
@@ -113,7 +114,10 @@ OUTPUT: TECH9;',
     file.copy(paste0(folder_path, "/", file_suffix_n, ".out"), datagen)
     file.remove(paste0(folder_path, "/", file_suffix_n, ".out"))
     
-    ## Write MplusAutomation scripts ##
+###############################    
+# Write MplusAutomation scripts
+###############################    
+    
     # Open the file for writing
     mpf <- paste0(scr_path, "/", f, n, "_r.inp")
     
@@ -125,7 +129,7 @@ filename = "%s_n%d_[[sample]].inp";
 outputDirectory = "%s";
 [[/init]]
 
-TITLE: CIE calculations for %s n=%d rep [[sample]];
+TITLE: ORourke & Han MC Simulation, Replication Analyses for %s n=%d rep [[sample]];
 DATA:
     File is %s%d_[[sample]].dat;
 
@@ -202,8 +206,9 @@ MODEL:
      
     for (i in 1:reps) {
       #Change filename depending on home/work location
-      #filename <- paste0("C..Users.HO.Wexford.Dropbox..ASU..Papers.O.Rourke...MZI2.R.R.Analyses.", f, ".N...", n, ".", tolower(f), "_n", n, "_", i, ".out")
-      filename <- paste0("C..Users.HO.Wexford.Desktop.test.", f, ".N...", n, ".", tolower(f), "_n", n, "_", i, ".out")
+      #Dot vs. slash convention is for MplusAutomation
+      #filename <- paste0("C..myfiles.home.", f, ".N...", n, ".", tolower(f), "_n", n, "_", i, ".out")
+      filename <- paste0("C..myfiles.work.", f, ".N...", n, ".", tolower(f), "_n", n, "_", i, ".out")
       ests_ <- get(params)[[filename]][["parameters"]][["unstandardized"]][["est"]]
       vec <- unlist(ests_)
       vec2 <- t(vec)
@@ -252,158 +257,13 @@ for (f in factors) {
   }
 }
 
-namestr <- paste0(ZINB_100_est[["C..Users.horourke.Dropbox..ASU..Papers.O.Rourke...MZI2.R.R.Analyses.ZINB.N...100.ZINB_n100.out"]][["parameters"]][["unstandardized"]][["paramHeader"]],
-                  " ",
-                  ZINB_100_est[["C..Users.horourke.Dropbox..ASU..Papers.O.Rourke...MZI2.R.R.Analyses.ZINB.N...100.ZINB_n100.out"]][["parameters"]][["unstandardized"]][["param"]] 
-                   )
-print(namestr)
-names(simests) <- c("cp_counts", "b_counts", "cp_zeroes", "b_zeroes", "a", "m_int", "y_z__int", "y_c_int", "m_resvar",
-                    "dispers", "x_0_c", "x_1_c", "erp0", "erp1", "li0", "x_0_z_old", "x_0_z_li", "x_1_z_old",
-                    "x_1_z_li", "rep", "factor", "n"
+names(simests) <- c("cp_counts", "b_counts", "cp_zeroes", "b_zeroes", "a", "m_int", "y_z_int", "y_c_int", "m_resvar",
+                    "dispers", "x_0_ab_c", "x_1_ab_c", "erp0", "erp1", "li0", "x_0_ab_ll", "x_0_ab_lg", "x_1_ab_ll",
+                    "x_1_ab_lg", "rep", "factor", "n"
                     )
 simests <- simests[,c(20,21,22,1:19)]
 simests <- subset(simests, select = -c(erp0,erp1,li0))
 
 #Save estimates data as one large .dat file
-write.table(simests, file="C:/Users/horourke/Dropbox (ASU)/Papers/O\'Rourke - MZI2/R&R/Analyses/all_ests.dat", row.names=FALSE, sep="\t", quote=FALSE)
-
-attach(simests)
-
-#Print tables of CIE means by sim condition
-aggregate(cbind(simests$x_0_c, simests$x_1_c,
-                simests$x_0_z_old, simests$x_0_z_li, 
-                simests$x_1_z_old, simests$x_1_z_li), 
-          list(simests$n, simests$factor), FUN=mean
-          ) 
-
-#Specify population values
-nrows <- nrow(simests)
-a_pop <- rep(0.5, nrows)
-b_z_pop <- rep(-0.2, nrows)
-b_c_pop <- rep(0.2, nrows)
-cp_z_pop <- rep(-1.4, nrows)
-cp_c_pop <- rep(1.4, nrows)
-m_int_pop <- rep(0, nrows)
-m_var_pop <- rep(1, nrows)
-y_intz_pop <- rep(0, nrows)
-y_intc_pop <- rep(3, nrows)
-disp <- rep(1, nrows)
-
-a <- simests$a
-zero_b <- simests$b_zeroes
-count_b <- simests$b_counts
-count_cp <- simests$cp_counts
-zero_cp <- simests$cp_zeroes
-count_i2 <- simests$y_c_int
-zero_i2 <- simests$y_z__int
-m2_mean <- simests$m_int
-med_c_0 = a * count_b * exp(count_i2 + (count_b * m2_mean) + (count_cp * 0))
-med_c_1 = a * count_b * exp(count_i2 + (count_b * m2_mean) + (count_cp * 1))
-exp0 <- exp(-(zero_i2 + (zero_b * m2_mean) + (zero_cp * 0)))
-exp1 <- exp(-(zero_i2 + (zero_b * m2_mean) + (zero_cp * 1)))
-new_med_z_0 = a * zero_b * (exp0 / ((1 + exp0)*(1 + exp0)))
-new_med_z_1 = a * zero_b * (exp1 / ((1 + exp1)*(1 + exp1)))
-old_med_z_0 = a * zero_b * exp(zero_i2 + (zero_b * m2_mean) + (zero_cp * 0))
-old_med_z_1 = a * zero_b * exp(zero_i2 + (zero_b * m2_mean) + (zero_cp * 1))
-
-#################
-# Population CIEs
-#################
-
-#for  counts, x = 0:
-med_c_0_pop = a_pop * b_c_pop * exp(y_intc_pop + (b_c_pop * m_int_pop) + (cp_c_pop * 0))
-
-#for  counts, x = 1:
-med_c_1_pop = a_pop * b_c_pop * exp(y_intc_pop + (b_c_pop * m_int_pop) + (cp_c_pop * 1))
-
-#exponentials for zeroes
-exp0_pop <- exp(-(y_intz_pop + (b_z_pop * m_int_pop) + (cp_z_pop * 0)))
-exp1_pop <- exp(-(y_intz_pop + (b_z_pop * m_int_pop) + (cp_z_pop * 1)))
-
-#for  zeroes, x = 0, old formula:
-old_med_z_0_pop = a_pop * b_z_pop * exp0_pop
-
-#for  zeroes, x = 1, old formula:
-old_med_z_1_pop = a_pop * b_z_pop * exp1_pop
-
-#for  zeroes, x = 0, new formula:
-new_med_z_0_pop = a_pop * b_z_pop * (exp0_pop / ((1 + exp0_pop)*(1 + exp0_pop)))
-
-#for  zeroes, x = 1, new formula:
-new_med_z_1_pop = a_pop * b_z_pop * (exp1_pop / ((1 + exp1_pop)*(1 + exp1_pop)))
-
-###pick up here calculating bias
-
-#Calculate bias - edit to calculate  bias and population values using R formulas from checks in Dec
-simests$a_bias_raw <- simests$a - a_pop
-simests$a_bias_rel <- simests$a_bias_raw/a_pop
-simests$b_z_bias_raw <- simests$b_zeroes - b_z_pop
-simests$b_z_bias_rel <- simests$b_z_bias_raw/b_z_pop
-
-simests$cie_z_0_bias_raw <- old_med_z_0 - new_med_z_0_pop
-simests$cie_z_0_bias_rel <- simests$cie_z_0_bias_raw/new_med_z_0_pop
-
-simests$cie_z_0_bias_raw2 <- new_med_z_0 - new_med_z_0_pop
-simests$cie_z_0_bias_rel2 <- simests$cie_z_0_bias_raw2/new_med_z_0_pop
-
-#Print tables of bias by sim condition
-aggregate(cbind(simests$cie_z_0_bias_raw, simests$cie_z_0_bias_rel), list(simests$n, simests$factor), FUN=mean) 
-
-aggregate(cbind(simests$cie_z_0_bias_raw2, simests$cie_z_0_bias_rel2), list(simests$n, simests$factor), FUN=mean) 
-
-#Print tables of CIE means by sim condition
-aggregate(cbind(simests$x_0_z_old, old_med_z_0_pop), 
-          list(simests$n, simests$factor), FUN=mean
-) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##delete if not needed
-# Move _list file to datagen folder before creating .inp files
-file.copy(paste0(folder_path, file_suffix_list, ".dat"), "C:/Users/HO Wexford/Dropbox (ASU)/Papers/O\'Rourke - MZI2/R&R/Analyses/datagen")
-file.remove(paste0(folder_path, file_suffix_list, ".dat"))
-
-# Create .inp files for all conditions and replications
-createModels(paste0(folder_path, file_suffix_r, ".inp"))
-
-#Move datagen and createmodels .inp/.out files out of condition folder
-file.copy(paste0(folder_path, file_suffix_r, ".inp"), "C:/Users/HO Wexford/Dropbox (ASU)/Papers/O\'Rourke - MZI2/R&R/Analyses/createmodels")
-file.remove(paste0(folder_path, file_suffix_r, ".inp"))
-file.copy(paste0(folder_path, file_suffix_n, ".inp"), "C:/Users/HO Wexford/Dropbox (ASU)/Papers/O\'Rourke - MZI2/R&R/Analyses/datagen")
-file.remove(paste0(folder_path, file_suffix_n, ".inp"))
-file.copy(paste0(folder_path, file_suffix_n, ".out"), "C:/Users/HO Wexford/Dropbox (ASU)/Papers/O\'Rourke - MZI2/R&R/Analyses/datagen")
-
-
-simests$id <- 1:nrow(simests)
-simests$factor <- vector(, nrow(simests))
-simests$factor[simests$id <= length(n_values)*reps] <- "ZINB"
-simests$factor[simests$id > length(n_values)*reps] <- "ZIP"
-simests$n <- vector(, nrow(simests))
-simests$n[simests$id <= reps] <- 100
-simests$n[simests$id > reps & simests$id <= reps*2] <- 250
-simests$n[simests$id > reps*2 & simests$id <= reps*3] <- 500
-simests$n[simests$id > reps*3 & simests$id <= reps*4] <- 750
-simests$n[simests$id > reps*4 & simests$id <= reps*5] <- 1500
-file.remove(paste0(folder_path, file_suffix_n, ".out"))
-
+write.table(simests, file=paste0(filepath, "all_ests.dat"), row.names=FALSE, sep="\t", quote=FALSE)
 
